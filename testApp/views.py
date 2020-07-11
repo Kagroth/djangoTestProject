61ae00759e7bd91a98b673194da8388a7c889d82
Kamil Bania <kamilbania2@gmail.com>
2020-07-11 17:22:14 +0200

    Model, widoki, template

--- testApp/views.py
+++ testApp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.conf import settings
+
+from .models import Directory
+import os
 
 # Create your views here.
 def index(request):
@@ -6,4 +10,28 @@ def index(request):
 
 
 def create_dir(request):
-    pass
+    path_to_create = settings.BASE_DIR + "/foldery/folder_utworzony_z_poziomu_aplikacji"
+
+    message = createDirectory(path_to_create)
+
+    return render(request, 'testApp/create_dir.html', {'message': message})
+
+
+def createDirectory(pathToCreate):
+    # funkcja tworzy folder w lokalizacji path/dirName
+    try:
+        directory = Directory.objects.create()
+        directory.name = "/folder_utworzony_z_poziomu_aplikacji" + str(directory.pk)
+        pathToCreate = pathToCreate + "-" + str(directory.pk)
+
+        if os.path.exists(pathToCreate):
+            os.rmdir(pathToCreate)
+        
+        os.mkdir(pathToCreate)
+
+        directory.save()
+        return True
+    except Exception as e:
+        print(str(e))
+        return False
+    
